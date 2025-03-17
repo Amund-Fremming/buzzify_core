e59dd7c8a19ba2b4a9474f3b62bd7dee84641c2c
Amund-fremming <amund.fremming@gmail.com>
2025-03-17 20:08:35 +0100

    yes

---
+++ Architecture/DependencyTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Xml.Linq;
+
+namespace Architecture;
+
+public class DependencyTests
+{
+    private static string GetProjectReference(string projectPath)
+    {
+        var doc = XDocument.Load(projectPath);
+        return string.Join(", ", doc.Descendants("ProjectReference")
+            .Select(x => x.Attribute("Include")?.Value ?? ""));
+    }
+
+    [Fact]
+    public void Presentation_Should_Only_Reference_Application()
+    {
+        var references = GetProjectReference("../../../../src/Presentation/Presentation.csproj");
+
+        Assert.DoesNotContain("Infrastructure.csproj", references);
+        Assert.Contains("Application.csproj", references);
+    }
+
+    [Fact]
+    public void Application_Should_Only_Reference_Domain()
+    {
+        var references = GetProjectReference("../../../../src/Application/Application.csproj");
+
+        Assert.DoesNotContain("Infrastructure.csproj", references);
+        Assert.DoesNotContain("Presentation.csproj", references);
+        Assert.Contains("Domain.csproj", references);
+    }
+
+    [Fact]
+    public void Infrastructure_Should_Only_Reference_Domain()
+    {
+        var references = GetProjectReference("../../../../src/Infrastructure/Infrastructure.csproj");
+
+        Assert.DoesNotContain("Application.csproj", references);
+        Assert.DoesNotContain("Presentation.csproj", references);
+        Assert.Contains("Domain.csproj", references);
+    }
+}
